9527e624ed6f63a5699a1686f0a44ecb0e21758e
ivanp <ipupkin@untref.edu.ar>
2023-07-08 22:58:45 -0300

    add draft for dr plot

--- plotting/dr_ratio.py
+++ plotting/dr_ratio.py
@@ -16,30 +16,21 @@ def filter_integration_time(string: str):
         raise ValueError(f"No match for integration time in {string}")
 
 
-df_objective = (
-    pl.read_csv(
-        "./data/results_LF-DR_objective.tsv", has_header=True, separator="\t"
-    )
-    .filter(
-        pl.col("dr_ratio").is_not_nan()
-        & pl.col("dr_ratio").is_not_null()
-    )
-)
+df_objective = pl.read_csv(
+    "./data/results_LF-DR_objective.tsv", has_header=True, separator="\t"
+).filter(pl.col("dr_ratio").is_not_nan() & pl.col("dr_ratio").is_not_null())
 
 print(
     "Objective DR statistics:\n",
     df_objective.select(
         pl.col("dr_ratio").mean().alias("dr_ratio_mean"),
         pl.col("dr_ratio").std().alias("dr_ratio_std"),
-    )
+    ),
 )
 
 df_subjective = (
     pl.read_csv("./data/results_LF-DR_subjective.tsv", has_header=True, separator="\t")
-    .filter(
-        pl.col("dr_ratio").is_not_nan()
-        & pl.col("dr_ratio").is_not_null()
-    )
+    .filter(pl.col("dr_ratio").is_not_nan() & pl.col("dr_ratio").is_not_null())
     .with_columns(
         pl.col("filename").apply(filter_integration_time).alias("integration_time")
     )
@@ -50,5 +41,11 @@ print(
     df_subjective.groupby("integration_time").agg(
         pl.col("dr_ratio").mean().alias("dr_ratio_mean"),
         pl.col("dr_ratio").std().alias("dr_ratio_std"),
-    )
+    ),
 )
+
+def plot_dr_vs_distance(df: pl.DataFrame, dr_column: str, distance_column: str) -> None:    
+    fig = px.line(df.to_numpy(), x=distance_column, y=dr_column)
+    # fig.update_layout(title="", yaxis_title="", xaxis_title="")
+    fig.show()
+
